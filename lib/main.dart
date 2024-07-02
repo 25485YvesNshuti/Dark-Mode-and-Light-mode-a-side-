d241deaec33c625b38f0752d8080441c1e150c0e
unknown <yvesnshuti25485@gmail.com>
2024-07-02 15:16:40 +0200

    darkmode and light mode

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkMode = false;
+  SharedPreferences? _prefs;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  bool get isDarkMode => _isDarkMode;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    _saveTheme();
+    notifyListeners();
+  }
+
+  Future<void> _loadTheme() async {
+    _prefs = await SharedPreferences.getInstance();
+    _isDarkMode = _prefs?.getBool('isDarkMode') ?? false;
+    notifyListeners();
+  }
+
+  Future<void> _saveTheme() async {
+    await _prefs?.setBool('isDarkMode', _isDarkMode);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode:
+              themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          home: MyHomePage(),
+        );
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Theme Example'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.brightness_6),
+            onPressed: () {
+              Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+    );
+  }
+}
